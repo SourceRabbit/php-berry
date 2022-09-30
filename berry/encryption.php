ad982d29fc80867d13bd28f5d1b18e587a596621
Nikos Siatras <nsiatras@gmail.com>
2022-09-30 11:17:15 +0300

    Encryption

---
+++ berry/encryption.php
@@ -0,0 +1,4 @@
+<?php
+require_once(__DIR__ . "/encryption/AES128Encryption.php");
+
+?>
--- berry/utils.php
+++ berry/utils.php
@@ -1,11 +1,4 @@
 <?php
 require_once(__DIR__ . "/utils/CrawlerDetector.php");
 
-$crawlerDetector = new CrawlerDetector();
-$crawlerName = $crawlerDetector->getCrawlerName();
-
-if ($crawlerName != "")
-{
-    print "Cralwer Detected: " . $crawlerDetector;
-}
 ?>
