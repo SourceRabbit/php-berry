2565367344fd3789f1fe85851d65e88a39442908
Nikos Siatras <nsiatras@gmail.com>
2022-09-30 15:27:19 +0300

    Comments removed

--- berry/mysql/MySQLCommand.php
+++ berry/mysql/MySQLCommand.php
@@ -1,36 +1,12 @@
 <?php
 
-/*
-  MIT License
-
-  Copyright (c) 2022 Nikos Siatras
-
-  Permission is hereby granted, free of charge, to any person obtaining a copy
-  of this software and associated documentation files (the "Software"), to deal
-  in the Software without restriction, including without limitation the rights
-  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-  copies of the Software, and to permit persons to whom the Software is
-  furnished to do so, subject to the following conditions:
-
-  The above copyright notice and this permission notice shall be included in all
-  copies or substantial portions of the Software.
-
-  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-  SOFTWARE.
- */
-
 class MySQLCommand
 {
 
     private MySQLConnection $fMySQLConnection;             // MySQLConnection of this command
     private String $fQuery;                                // Query to execute
     private $fPreparedStatement = NULL;                    // The prepared statement of this command
-    public  $Parameters = NULL;      // Command parameters
+    public MySQLCommandParameters $Parameters;            // Command parameters
 
     /**
      * Constructs a new MySQLCommand
@@ -40,7 +16,7 @@ class MySQLCommand
     public function __construct(MySQLConnection $mySQLConnection, String $query = "")
     {
         $this->fMySQLConnection = $mySQLConnection;
-        $this->Parameters = new MySQLCommandParameters($this);
+        $this->Parameters = new MySQLCommandParameters();
         $this->fQuery = $query;
     }
 
