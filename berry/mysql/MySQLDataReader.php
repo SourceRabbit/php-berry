38eac63bff62e7a5865b4022c9a4bf93c57dde47
Nikos Siatras <nsiatras@gmail.com>
2024-10-16 19:42:13 +0300

    MySQL refactored

--- berry/mysql/MySQLDataReader.php
+++ berry/mysql/MySQLDataReader.php
@@ -75,15 +75,40 @@ class MySQLDataReader
     }
 
     /**
-     * Return a readed value
+     * Return a read value
      * @param type $index
      * @return type 
      */
-    public function getValue($index)
+    public function getValue(int $index)
     {
         return $this->fReadValues[$index];
     }
 
+    /**
+     * DATETIME is always stored and returned as UTC, regardless of what your system's timezone is.
+     * This method reads a MySQL DATETIME value into a PHP DateTime Object and converts the TimeZone
+     * to the given TimeZone.
+     * @param int $index
+     * @param DateTimeZone $timeZone 
+     * @return DateTime
+     */
+    public function getDateTime(int $index, DateTimeZone $timeZone): DateTime
+    {
+        $dt = new DateTime($this->fReadValues[$index], new DateTimeZone('UTC'));
+        $dt->setTimezone($timeZone);
+        return $dt;
+    }
+
+    /**
+     * Return the read datetime value
+     * @param int $index
+     * @return DateTimeF
+     */
+    public function getDateTimeUTC(int $index): DateTime
+    {
+        $dt = new DateTime($this->fReadValues[$index]);
+        return $dt;
+    }
 }
 
 ?>
